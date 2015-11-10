cc3424dc2c43537e4ddfc579e2f50e781b20fad0
Thorsten Schuett <schuett@zib.de>
2015-11-10 16:01:15 +0100

    renamed test-distributions.sh

--- test-distribution.sh
+++ test-distribution.sh
@@ -19,28 +19,28 @@ docker pull centos
 docker pull fedora
 docker pull debian
 
+
+DISTRIBUTION_DIR=$1
 ret=0
 failed=""
 
 #run containers
-for DISTRIBUTION_DIR in $DISTRIBUTION_DIRS; do
 
-   # build docker image
-   docker build -t xtreemfs/$DISTRIBUTION_DIR $DIR/$DISTRIBUTION_DIR
+# build docker image
+docker build -t scalaris/$DISTRIBUTION_DIR $DIR/$DISTRIBUTION_DIR
 
-   # run docker container
-   docker run --name=$CONTAINER_NAME --privileged xtreemfs/$DISTRIBUTION_DIR ./test.sh
+# run docker container
+docker run --name=$CONTAINER_NAME --privileged scalaris/$DISTRIBUTION_DIR ./test.sh
 
-   if [ $? -ne 0 ]; then
-       ret=`expr $ret + 1`
-       echo "$DISTRIBUTION_DIR test failed!"
-       failed=$(printf "%s\n%s" "$failed" "$DISTRIBUTION_DIR")
-   fi
+if [ $? -ne 0 ]; then
+    ret=`expr $ret + 1`
+    echo "$DISTRIBUTION_DIR test failed!"
+    failed=$(printf "%s\n%s" "$failed" "$DISTRIBUTION_DIR")
+fi
 
-   #remove container and image
-   docker rm $CONTAINER_NAME
-   docker rmi xtreemfs/$DISTRIBUTION_DIR
-done
+#remove container and image
+docker rm $CONTAINER_NAME
+docker rmi xtreemfs/$DISTRIBUTION_DIR
 
 if [ $ret -ne 0 ]; then
     echo "The following tests failed: $failed"
