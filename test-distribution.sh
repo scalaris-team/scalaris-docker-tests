0d76726e36e33ea3f4c89340aaa46c17a013bfbd
Thorsten Schuett <schuett@zib.de>
2015-11-10 17:05:58 +0100

    remove dead code

--- test-distribution.sh
+++ test-distribution.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# usage: ./test-distributions.sh [ all| [distribution]* ]
-# default: ./test.distibutions.sh all
-
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/distributions
 CONTAINER_NAME="test-container"
-
-# prepare distribution dirs
-if [[ "$1" == "all" || "$1" == "" ]]; then
-   DISTRIBUTION_DIRS=$(ls $DIR | grep "xtreemfs-")
-else
-  for i in $*; do DISTRIBUTION_DIRS="$DISTRIBUTION_DIRS $(ls $DIR | grep "xtreemfs-$i")"; done
-fi
 
 docker pull ubuntu
 docker pull opensuse
@@ -23,8 +13,6 @@ docker pull debian
 DISTRIBUTION_DIR=$1
 ret=0
 failed=""
-
-#run containers
 
 # build docker image
 docker build -t scalaris/$DISTRIBUTION_DIR $DIR/$DISTRIBUTION_DIR
