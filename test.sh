0ff483203d640385723a0201dd506f72d8d7c49c
Nico Kruber <kruber@zib.de>
2016-02-29 16:06:11 +0100

    skip systemd tests (not supported with docker)

--- test.sh
+++ test.sh
@@ -38,6 +38,7 @@ scalarisctl $START_TYPE_FIRST $NODE_NAME -m -d stop || echo -e "\x1b[1;31m##### 
 sleep 5s # wait for Scalaris to stop
 
 number_nodes=1
+skip_system_scalaris=no
 if [ -x /etc/init.d/scalaris ]; then
   if [ -f /etc/init.d/scalaris-first ]; then
     number_nodes=2
@@ -49,54 +50,59 @@ if [ -x /etc/init.d/scalaris ]; then
   sed -e 's/SCALARIS_NODE=.*/SCALARIS_NODE="node1@127.0.0.1"/g' -i /etc/scalaris/initd.conf
   /etc/init.d/scalaris start || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris via init.d script #####\x1b[0m"; cat /var/log/scalaris/initd_node1.log )
 else
-  if [ -f /etc/conf.d/scalaris-first ]; then
-    number_nodes=2
-    sed -e 's/SCALARIS_NODE=.*/SCALARIS_NODE=node@127.0.0.1/g' -i /etc/conf.d/scalaris-first
-    systemctl start scalaris-first.service || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris-first via systemctl #####\x1b[0m"; journalctl -xn50 --no-pager )
-  else
-    echo "{first, true}." >> /etc/scalaris/scalaris.local.cfg
-  fi
-  sed -e 's/SCALARIS_NODE=.*/SCALARIS_NODE=node1@127.0.0.1/g' -i /etc/conf.d/scalaris
-  systemctl start scalaris.service || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris via systemctl #####\x1b[0m"; journalctl -xn50 --no-pager )
+  echo -e "\x1b[1;34mATTENTION: systemd not supported with docker\x1b[0m" # in blue
+  skip_system_scalaris=yes
+#   if [ -f /etc/conf.d/scalaris-first ]; then
+#     number_nodes=2
+#     sed -e 's/SCALARIS_NODE=.*/SCALARIS_NODE=node@127.0.0.1/g' -i /etc/conf.d/scalaris-first
+#     systemctl start scalaris-first.service || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris-first via systemctl #####\x1b[0m"; journalctl -xn50 --no-pager )
+#   else
+#     echo "{first, true}." >> /etc/scalaris/scalaris.local.cfg
+#   fi
+#   sed -e 's/SCALARIS_NODE=.*/SCALARIS_NODE=node1@127.0.0.1/g' -i /etc/conf.d/scalaris
+#   systemctl start scalaris.service || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris via systemctl #####\x1b[0m"; journalctl -xn50 --no-pager )
 fi
-sleep 10s # wait for Scalaris to start
 
-mkdir -p log/initd && cd log/initd
-wget --timeout 5 -q --page-requisites http://localhost:8000/indexed-ring.yaws
-wget_download=$?
-mv "localhost:8000/indexed-ring.yaws" "localhost:8000/indexed-ring.html"
-if [ $wget_download -gt 0 ] ; then
-  echo -e "\x1b[1;31mScalaris web debug interface is not running (after starting via init.d/systemctl). Exit\x1b[0m"
-  result=1
-fi
-if [ "${number_nodes}" -eq 2 ] ; then
-  wget --timeout 5 -q --page-requisites http://localhost:8001/ring.yaws
+if [ "$skip_system_scalaris" = "no" ]; then
+  sleep 10s # wait for Scalaris to start
+
+  mkdir -p log/initd && cd log/initd
+  wget --timeout 5 -q --page-requisites http://localhost:8000/indexed-ring.yaws
   wget_download=$?
-  mv "localhost:8001/ring.yaws" "localhost:8001/ring.html"
+  mv "localhost:8000/indexed-ring.yaws" "localhost:8000/indexed-ring.html"
   if [ $wget_download -gt 0 ] ; then
-    echo -e "\x1b[1;31mScalaris-first web debug interface is not running (after starting via init.d/systemctl). Exit\x1b[0m"
+    echo -e "\x1b[1;31mScalaris web debug interface is not running (after starting via init.d/systemctl). Exit\x1b[0m"
     result=1
   fi
-fi
-cd -
+  if [ "${number_nodes}" -eq 2 ] ; then
+    wget --timeout 5 -q --page-requisites http://localhost:8001/ring.yaws
+    wget_download=$?
+    mv "localhost:8001/ring.yaws" "localhost:8001/ring.html"
+    if [ $wget_download -gt 0 ] ; then
+      echo -e "\x1b[1;31mScalaris-first web debug interface is not running (after starting via init.d/systemctl). Exit\x1b[0m"
+      result=1
+    fi
+  fi
+  cd -
 
-if [ -x /etc/init.d/scalaris ]; then
-  if [ "${number_nodes}" -eq 2 ] ; then
-    /etc/init.d/scalaris stop || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris via init.d script #####\x1b[0m"; result=1 )
-    /etc/init.d/scalaris-first kill || ( echo -e "\x1b[1;31m##### FAILED to kill Scalaris-first via init.d script #####\x1b[0m"; result=1 )
+  if [ -x /etc/init.d/scalaris ]; then
+    if [ "${number_nodes}" -eq 2 ] ; then
+      /etc/init.d/scalaris stop || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris via init.d script #####\x1b[0m"; result=1 )
+      /etc/init.d/scalaris-first kill || ( echo -e "\x1b[1;31m##### FAILED to kill Scalaris-first via init.d script #####\x1b[0m"; result=1 )
+    else
+      /etc/init.d/scalaris kill || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris via init.d script #####\x1b[0m"; result=1 )
+    fi
+    if [ "${result}" -ne 0 ] ; then
+      cat /var/log/scalaris/initd_node*.log
+    fi
   else
-    /etc/init.d/scalaris kill || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris via init.d script #####\x1b[0m"; result=1 )
-  fi
-  if [ "${result}" -ne 0 ] ; then
-    cat /var/log/scalaris/initd_node*.log
-  fi
-else
-  systemctl stop scalaris.service || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris via systemctl #####\x1b[0m"; result=1 )
-  if [ "${number_nodes}" -eq 2 ] ; then
-    systemctl stop scalaris-first.service || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris-first via systemctl #####\x1b[0m"; result=1 )
-  fi
-  if [ "${result}" -ne 0 ] ; then
-    journalctl -xn100 --no-pager
+    systemctl stop scalaris.service || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris via systemctl #####\x1b[0m"; result=1 )
+    if [ "${number_nodes}" -eq 2 ] ; then
+      systemctl stop scalaris-first.service || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris-first via systemctl #####\x1b[0m"; result=1 )
+    fi
+    if [ "${result}" -ne 0 ] ; then
+      journalctl -xn100 --no-pager
+    fi
   fi
 fi
 
