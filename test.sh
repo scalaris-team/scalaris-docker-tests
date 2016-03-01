212fe23a8a538361fddfeee04388d8cf933e69ca
Nico Kruber <kruber@zib.de>
2016-03-01 12:06:28 +0100

    lett output from test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 /sbin/ifconfig
 
@@ -14,7 +14,9 @@ else
   START_TYPE_FIRST="-f -s"
 fi
 
+echo "Running scalarisctl checkinstallation..."
 scalarisctl $NODE_NAME checkinstallation || result=1
+echo "Starting run-time tests with scalarisctl..."
 scalarisctl $START_TYPE_FIRST $NODE_NAME -m -d start || echo -e "\x1b[1;31m##### FAILED to start Scalaris #####\x1b[0m"
 
 sleep 5s # wait for Scalaris to start
@@ -28,8 +30,10 @@ wget --timeout 5 -q --page-requisites http://localhost:8000/indexed-ring.yaws
 wget_download=$?
 mv "localhost:8000/indexed-ring.yaws" "localhost:8000/indexed-ring.html"
 if [ $wget_download -gt 0 ] ; then
-  echo -e "\x1b[1;31mScalaris web debug interface is not running. Exit\x1b[0m"
+  echo -e "\x1b[1;31mScalaris web debug interface is not running (port 8000). Exit\x1b[0m"
   result=1
+else
+  echo -e "Scalaris web debug interface running (port 8000). OK so far"
 fi
 cd -
 
@@ -40,27 +44,29 @@ sleep 5s # wait for Scalaris to stop
 number_nodes=1
 skip_system_scalaris=no
 if [ -x /etc/init.d/scalaris ]; then
+echo "Starting run-time tests with init.d..."
   if [ -f /etc/init.d/scalaris-first ]; then
     number_nodes=2
     sed -e 's/SCALARIS_NODE=.*/SCALARIS_NODE="node@127.0.0.1"/g' -i /etc/scalaris/initd-first.conf
-    /etc/init.d/scalaris-first start || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris-first via init.d script #####\x1b[0m"; cat /var/log/scalaris/initd_node.log )
+    /etc/init.d/scalaris-first start || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris-first #####\x1b[0m"; cat /var/log/scalaris/initd_node.log )
   else
     echo "{first, true}." >> /etc/scalaris/scalaris.local.cfg
   fi
   sed -e 's/SCALARIS_NODE=.*/SCALARIS_NODE="node1@127.0.0.1"/g' -i /etc/scalaris/initd.conf
-  /etc/init.d/scalaris start || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris via init.d script #####\x1b[0m"; cat /var/log/scalaris/initd_node1.log )
+  /etc/init.d/scalaris start || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris #####\x1b[0m"; cat /var/log/scalaris/initd_node1.log )
 else
   echo -e "\x1b[1;34mATTENTION: systemd not supported with docker\x1b[0m" # in blue
   skip_system_scalaris=yes
+#   echo "Starting run-time tests with systemctl..."
 #   if [ -f /etc/conf.d/scalaris-first ]; then
 #     number_nodes=2
 #     sed -e 's/SCALARIS_NODE=.*/SCALARIS_NODE=node@127.0.0.1/g' -i /etc/conf.d/scalaris-first
-#     systemctl start scalaris-first.service || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris-first via systemctl #####\x1b[0m"; journalctl -xn50 --no-pager )
+#     systemctl start scalaris-first.service || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris-first #####\x1b[0m"; journalctl -xn50 --no-pager )
 #   else
 #     echo "{first, true}." >> /etc/scalaris/scalaris.local.cfg
 #   fi
 #   sed -e 's/SCALARIS_NODE=.*/SCALARIS_NODE=node1@127.0.0.1/g' -i /etc/conf.d/scalaris
-#   systemctl start scalaris.service || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris via systemctl #####\x1b[0m"; journalctl -xn50 --no-pager )
+#   systemctl start scalaris.service || ( echo -e "\x1b[1;31m##### FAILED to start Scalaris #####\x1b[0m"; journalctl -xn50 --no-pager )
 fi
 
 if [ "$skip_system_scalaris" = "no" ]; then
@@ -71,39 +77,47 @@ if [ "$skip_system_scalaris" = "no" ]; then
   wget_download=$?
   mv "localhost:8000/indexed-ring.yaws" "localhost:8000/indexed-ring.html"
   if [ $wget_download -gt 0 ] ; then
-    echo -e "\x1b[1;31mScalaris web debug interface is not running (after starting via init.d/systemctl). Exit\x1b[0m"
+    echo -e "\x1b[1;31mScalaris web debug interface is not running (port 8000). Exit\x1b[0m"
     result=1
+  else
+    echo -e "Scalaris web debug interface running (port 8000). OK so far"
   fi
   if [ "${number_nodes}" -eq 2 ] ; then
     wget --timeout 5 -q --page-requisites http://localhost:8001/ring.yaws
     wget_download=$?
     mv "localhost:8001/ring.yaws" "localhost:8001/ring.html"
     if [ $wget_download -gt 0 ] ; then
-      echo -e "\x1b[1;31mScalaris-first web debug interface is not running (after starting via init.d/systemctl). Exit\x1b[0m"
+      echo -e "\x1b[1;31mScalaris-first web debug interface is not running (port 8001). Exit\x1b[0m"
       result=1
+    else
+      echo -e "Scalaris-first web debug interface running (port 8001). OK so far"
     fi
   fi
   cd -
 
   if [ -x /etc/init.d/scalaris ]; then
     if [ "${number_nodes}" -eq 2 ] ; then
-      /etc/init.d/scalaris stop || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris via init.d script #####\x1b[0m"; result=1 )
-      /etc/init.d/scalaris-first kill || ( echo -e "\x1b[1;31m##### FAILED to kill Scalaris-first via init.d script #####\x1b[0m"; result=1 )
+      /etc/init.d/scalaris stop || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris #####\x1b[0m"; result=1 )
+      /etc/init.d/scalaris-first kill || ( echo -e "\x1b[1;31m##### FAILED to kill Scalaris-first #####\x1b[0m"; result=1 )
     else
-      /etc/init.d/scalaris kill || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris via init.d script #####\x1b[0m"; result=1 )
+      /etc/init.d/scalaris kill || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris #####\x1b[0m"; result=1 )
     fi
     if [ "${result}" -ne 0 ] ; then
       cat /var/log/scalaris/initd_node*.log
     fi
   else
-    systemctl stop scalaris.service || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris via systemctl #####\x1b[0m"; result=1 )
+    systemctl stop scalaris.service || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris #####\x1b[0m"; result=1 )
     if [ "${number_nodes}" -eq 2 ] ; then
-      systemctl stop scalaris-first.service || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris-first via systemctl #####\x1b[0m"; result=1 )
+      systemctl stop scalaris-first.service || ( echo -e "\x1b[1;31m##### FAILED to stop Scalaris-first #####\x1b[0m"; result=1 )
     fi
     if [ "${result}" -ne 0 ] ; then
       journalctl -xn100 --no-pager
     fi
   fi
+fi
+
+if [ "${result}" -eq 0 ] ; then
+  echo -e "\x1b[1;32mSUCCESS: All tests passed\x1b[0m" #green
 fi
 
 exit $result
